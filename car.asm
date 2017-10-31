ade731294518d32af9238ea311d60c9a483dd70e
Luiz Eduardo Dorici <luizdorici@gmail.com>
2017-10-30 22:05:23 -0200

    Tela inicial, espaço sempre começa o jogo

--- car.asm
+++ car.asm
@@ -2,38 +2,55 @@
 jmp main
 
 main:
-	loadn r1, #tela1linha0 ; endereco da primeira linha da tela
+	loadn r1, #starttela1linha0 ; endereco da primeira linha da tela
 	loadn r7, #0 ;inicializacao de flags
 	store colisaoflag, r7
-	loadn r6, #120 ;criterio de parada da impressao de telas, ultima tela a ser impressa, voltar para a primeira
+	loadn r6, #40 ;criterio de parada da impressao de telas, ultima tela a ser impressa, voltar para a primeira
 	loadn r3, #31  ;incremento da coluna da tela, para shifitar a tela
-	loadn r5, #32  ;tecla espaco
+	;loadn r5, #32  ;tecla espaco
 	loadn r4, #643 ;posicao inicial do carrinho na tela -200 sobe a rua +200 desce
 	store atualpos, r4 ;armazena posição inicial do carrinho na variavel global atualpos
 	loadn r4, #12
 	store altura, r4 ;altura inicial do carro na tela logica
 	
+	
+	;===============tela inicial================
 	loadn r5, #32  ;tecla espaco
-	loadn r2, #1536 ;cor do mapa
-	call imprimeTela
-	loadn r2, #0 ;cor da escrita na tela
+	
+	loadn r2, #768 ;cor do mapa
+	call imprimeTelaInit
+	
+	loadn r4, #327
+	store cursor, r4
+	
+	loadn r2, #0
+	loadn r0, #321
 	loadn r1, #start
-    call Imprimestr
-	loadn r1, #tela1linha0
+	call Imprimestr
+	loadn r0, #441
+	loadn r1, #selection
+	call Imprimestr
+	loadn r2, #3584
+	loadn r0, #121
+	loadn r1, #gamename
+	call Imprimestr
+	
+	loadn r5, #'>'
+	load r4, cursor
+	
+	outchar r5, r4
+	
+	call movecursor
+	
+	;==============fim tela inicial===============
 	loadn r2, #1536 ;cor do mapa
-	loadn r0, #39
-
-	;loop tela inicial antes de apertar espaco para comecar
-	loopinit:
-		inchar r4
-		cmp r4, r5
-	jne loopinit
+	loadn r1, #tela1linha0 ; endereco da primeira linha da tela
 	loadn r5, #1
+	loadn r6, #160 ;criterio de parada da impressao de telas, ultima tela a ser impressa, voltar para a primeira
 	
 	
 		loop:
 		store screen, r1
-		;call colisao
 		call imprimeTela
 		load r4, colisaoflag
 		cmp r4, r5
@@ -54,6 +71,7 @@ main:
 	endfunc:
 		loadn r1, #gameover
 		loadn r0, #81
+		loadn r2, #0
 		call Imprimestr
 		loadn r5, #32
 		loopend:
@@ -63,7 +81,95 @@ main:
 		jmp main
 			
 	halt
+
+
+movecursor:
+	push r0
+	push r1
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
 	
+	loadn r0, #327
+	loadn r1, #447
+	loadn r5, #32  ;tecla espaco
+	
+	loadn r3, #119
+	loadn r4, #115
+	
+	cursorloop:
+		inchar r2 ;captura tecla da tela para verificar se troca via da pista
+		cmp r2, r3
+		ceq cursorup
+	
+		cmp r2, r4
+		ceq cursordown
+	
+		cmp r2, r5
+		jeq checkPosition
+		jmp cursorloop
+		
+	checkPosition:
+		;load r6, cursor
+		;cmp r0, r6
+		;jeq 
+		;cmp r1, r6
+		;jeq
+		
+	
+	;loadn r1, #'>'
+	;load r0, cursor
+	;outchar r1, r0
+	
+movecursorFim:
+	pop r6
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	rts
+	
+cursorup:
+	push r0
+	push r1
+	
+	loadn r1, #' '
+	
+	load r0, cursor
+	outchar r1, r0
+	
+	loadn r0, #327
+	loadn r1, #'>'
+	
+	outchar r1, r0
+	store cursor, r0
+	
+	pop r1
+	pop r0
+	rts
+	
+cursordown:
+	push r0
+	push r1
+	
+	loadn r1, #' '
+	
+	load r0, cursor
+	outchar r1, r0
+	
+	loadn r0, #447
+	loadn r1, #'>'
+	
+	outchar r1, r0
+	store cursor, r0
+	
+	pop r1
+	pop r0
+	rts
 	
 colisaoactive:
 	push r0
@@ -262,6 +368,43 @@ imprimeTela:
 	pop r0
 	
 	rts
+	
+	
+imprimeTelaInit:
+	push r0
+	push r1
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
+	push r7
+
+	loadn r0, #0 ; pos inicial OBS: TEM QUE SER O COMECO DA Tela
+	loadn r3, #1 ; Incremento da pos da tela
+	loadn r4, #31 ; Incremento do ponteiro das linhas da tela
+	loadn r5, #40 ; Limite da tela, criterio de parada
+
+ImprimeTelaInitLoop:
+	call Imprimecoluna
+	
+	add r0, r0, r3 ; Incrementa pos para a segunda linha na tela --> r0 = r0 + 1
+	add r1, r1, r4 ; incrementa o ponteiro para o comeco da proxima linha na memoria (30 + 1 ...)
+	cmp r0, r5 ; compara r0 com 40
+	jne ImprimeTelaInitLoop ; Enquanto r0 < 40
+	
+
+	pop r7
+	pop r6
+	pop	r5
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+
+	rts
+
 
 Imprimestr:	;  Rotina de Impresao de Mensagens:    r0 = Posicao da tela que o primeiro caractere da mensagem sera' impresso;  r1 = endereco onde comeca a mensagem; r2 = cor da mensagem.   Obs: a mensagem sera' impressa ate' encontrar "/0"
 	push r0	; protege o r0 na pilha para preservar seu valor
@@ -340,10 +483,59 @@ decy:
 flagrodar: var #1
 atualpos: var #1
 gameover: string "            GAME OVER                   "
-start: string "       Press Space To Start             "
+gamename: string "        NO IDEA FOR A NAME GAME       "
+start: string "         Iniciar um novo jogo         "
+selection: string "         Customuze seu carro          "
+
 screen: var #1 
 colisaoflag: var #1
 altura: var #1
+score: var #1
+cursor: var #1
+
+
+
+starttela1linha0:   string "##############################"
+starttela1linha1:   string "#                            #"
+starttela1linha2:   string "#                            #"
+starttela1linha3:   string "#                            #"
+starttela1linha4:   string "#                            #"
+starttela1linha5:   string "#                            #"
+starttela1linha6:   string "#                            #"
+starttela1linha7:   string "#                            #"
+starttela1linha8:   string "#                            #"
+starttela1linha9:   string "#                            #"
+starttela1linha10:  string "#                            #"
+starttela1linha11:  string "#                            #"
+starttela1linha12:  string "#                            #"
+starttela1linha13:  string "#                            #"
+starttela1linha14:  string "#                            #"
+starttela1linha15:  string "#                            #"
+starttela1linha16:  string "#                            #"
+starttela1linha17:  string "#                            #"
+starttela1linha18:  string "#                            #"
+starttela1linha19:  string "#                            #"
+starttela1linha20:  string "#                            #"
+starttela1linha21:  string "#                            #"
+starttela1linha22:  string "#                            #"
+starttela1linha23:  string "#                            #"
+starttela1linha24:  string "#                            #"
+starttela1linha25:  string "#                            #"
+starttela1linha26:  string "#                            #"
+starttela1linha27:  string "#                            #"
+starttela1linha28:  string "#                            #"
+starttela1linha29:  string "#                            #"
+starttela1linha30:  string "#                            #"
+starttela1linha31:  string "#                            #"
+starttela1linha32:  string "#                            #"
+starttela1linha33:  string "#                            #"
+starttela1linha34:  string "#                            #"
+starttela1linha35:  string "#                            #"
+starttela1linha36:  string "#                            #"
+starttela1linha37:  string "#                            #"
+starttela1linha38:  string "#                            #"
+starttela1linha39:  string "##############################"
+
 
 tela1linha0:   string "          -    -    -    -    "
 tela1linha1:   string "          -              -    "
@@ -549,6 +741,47 @@ tela1linha196:	string "          -    -    -    -    "
 tela1linha197:	string "          -              -    "
 tela1linha198:	string "          -    -    -    -    "
 tela1linha199:	string "          -              -    "
+
+tela1linha200:	string "          -    -    -    -    "
+tela1linha201:	string "          -              -    "
+tela1linha202:	string "          -    -    -    -    "
+tela1linha203:	string "          -              -    "
+tela1linha204:	string "          -    -    -    -    "
+tela1linha205:	string "          -              -    "
+tela1linha206:	string "          -    -    -    -    "
+tela1linha207:	string "          -              -    "
+tela1linha208:	string "          -    -    -    -    "
+tela1linha209:	string "          -              -    "
+tela1linha210:	string "          -    -    -    -    "
+tela1linha211:	string "          -              -    "
+tela1linha212:	string "          -    -    -    -    "
+tela1linha213:	string "          -              -    "
+tela1linha214:	string "          -    -    -    -    "
+tela1linha215:	string "          -              -    "
+tela1linha216:	string "          -    -    -    -    "
+tela1linha217:	string "          -              -    "
+tela1linha218:	string "          -    -    -    -    "
+tela1linha219:	string "          -              -    "
+tela1linha220:	string "          -    -    -    -    "
+tela1linha221:	string "          -              -    "
+tela1linha222:	string "          -    -    -    -    "
+tela1linha223:	string "          -              -    "
+tela1linha224:	string "          -    -    -    -    "
+tela1linha225:	string "          -              -    "
+tela1linha226:	string "          -    -    -    -    "
+tela1linha227:	string "          -              -    "
+tela1linha228:	string "          -    -    -    -    "
+tela1linha229:	string "          -              -    "
+tela1linha230:	string "          -    -    -    -    "
+tela1linha231:	string "          -              -    "
+tela1linha232:	string "          -    -    -    -    "
+tela1linha233:	string "          -              -    "
+tela1linha234:	string "          -    -    -    -    "
+tela1linha235:	string "          -              -    "
+tela1linha236:	string "          -    -    -    -    "
+tela1linha237:	string "          -              -    "
+tela1linha238:	string "          -    -    -    -    "
+tela1linha239:	string "          -              -    "
 
 carlinha0:	string " +@ - @| "
 carlinha1:	string "[<[##]>>]"
